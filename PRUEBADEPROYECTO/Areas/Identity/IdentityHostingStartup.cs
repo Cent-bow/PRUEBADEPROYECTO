e084501d6bb0a3f99323d9231849b80a16ffa739
Centhia Bowman <Centhiabowman@outlook.com>
2021-05-13 18:37:44 -0400

    Agregar archivos de proyecto.

---
+++ PRUEBADEPROYECTO/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PRUEBADEPROYECTO.Data;
+
+[assembly: HostingStartup(typeof(PRUEBADEPROYECTO.Areas.Identity.IdentityHostingStartup))]
+namespace PRUEBADEPROYECTO.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ PRUEBADEPROYECTO/Data/Migrations/20210513220209_primeraMigracion.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PRUEBADEPROYECTO.Data.Migrations
+{
+    public partial class primeraMigracion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
