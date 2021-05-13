c6e689fcb634b531e4875a2bea415f745f00ba5c
Centhia Bowman <Centhiabowman@outlook.com>
2021-05-13 19:27:55 -0400

    producto

---
+++ PRUEBADEPROYECTO/Models/Producto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PRUEBADEPROYECTO.Models
+{
+    public class Producto
+    {
+    }
+}
