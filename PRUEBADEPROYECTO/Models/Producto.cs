04953b7e35ed8ae3e71e287351d28d84d1668da2
Centhia Bowman <Centhiabowman@outlook.com>
2021-05-13 19:37:17 -0400

    producto

--- PRUEBADEPROYECTO/Models/Producto.cs
+++ PRUEBADEPROYECTO/Models/Producto.cs
@@ -7,5 +7,16 @@ namespace PRUEBADEPROYECTO.Models
 {
     public class Producto
     {
+
+        public int Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string Precio { get; set; }
+        public string Cantidad { get; set; }
+        public string Imagen { get; set; }
+
+
+
+
     }
 }
